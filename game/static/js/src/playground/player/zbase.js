627d3b7091a98d93e1e4c371292d46ab4f544d2b
LIU  JIAHAO <1059723379@qq.com>
2022-12-08 12:21:29 +0800

    add player

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -7,6 +7,7 @@ class Player extends AcGameObject {
         this.y = y;
         this.vx = 0;
         this.vy = 0;
+        this.move_length = 0;
         this.radius = radius;
         this.color = color;
         this.speed = speed;
@@ -22,11 +23,44 @@ class Player extends AcGameObject {
     }
 
     add_listening_events() {
+        let outer = this;
+        this.playground.game_map.$canvas.on("contextmenu", function(){ //取消右键的菜单
+            return false;
+        });
+        this.playground.game_map.$canvas.mousedown(function(e) { //获取右键点击的坐标
+            if (e.which === 3) {
+                outer.move_to(e.clientX, e.clientY);
+
+            }
+
+        });
+    }
+
+    get_dist(x1, y1, x2, y2) {
+        let dx = x1 - x2;
+        let dy = y1 - y2;
+        return Math.sqrt(dx * dx + dy * dy);
+
+    }
+
+    move_to(tx, ty) {
+        this.move_length = this.get_dist(this.x, this.y, tx, ty); //移动的模长
+        let angle = Math.atan2(ty - this.y, tx - this.x); //求移动向量的角度
+        this.vx = Math.cos(angle); //表示速度，其实是1*cos(angle)
+        this.vy = Math.sin(angle);
+
     }
 
     update() {
-        this.x += this.vx;
-        this.y += this.vy;
+        if (this.move_length < this.eps) {
+            this.move_length = 0;
+            this.vx = this.vy = 0;
+        } else {
+            let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000); //不能移出界，moved表示的是每秒真实移动的距离
+            this.x += this.vx * moved;
+            this.y += this.vy * moved;
+            this.move_length -= moved;
+        }
         this.render();
     }
 
